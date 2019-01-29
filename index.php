8883784046bf575ed70d08375c3c6f59be97c50d
Eric Horodyski <eric@horodyski.me>
2019-01-28 19:12:50 -0500

    Styling Update

--- index.php
+++ index.php
@@ -1,10 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>PHP: Sent Headers/Cookies Echo</title>
+</head>
+<body>
+  <h1>Request Headers</h1>
+  <table>
+    <tr>
+      <thead>
+        <td>Header</td>
+        <td>Value</td>
+      </thead>
+    </tr>
 
-$headers = apache_request_headers();
-foreach ($headers as $header => $value) {
- echo "<pre>";
- echo "$header : $value";
- echo "</pre>";
-}
+  </table>
 
-?>
+
+  <?php
+
+  $headers = apache_request_headers();
+  foreach ($headers as $header => $value) {
+  echo "<pre>";
+  echo "$header : $value";
+  echo "</pre>";
+  }
+
+  ?>
+</body>
+</html>
+
