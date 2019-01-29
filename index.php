a440b61889af9329473871f9900eab7825d8957e
Eric Horodyski <eric@horodyski.me>
2019-01-28 19:26:33 -0500

    Style updates

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>PHP: Sent Headers/Cookies Echo</title>
+  <style>
+    body { font-family: Arial, Helvetica, sans-serif; }
+    thead { font-weight: 800; }
+  </style>
 </head>
 <body>
   <h1>Request Headers</h1>
